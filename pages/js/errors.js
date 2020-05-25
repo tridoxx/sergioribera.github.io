6b841a063f642b71c28ddf8248b79ad05e1aa7f7
SergioRibera <sergioalejandroriberacosta@gmail.com>
2020-05-25 19:30:34 -0400

    Fixed Bugs

---
+++ pages/js/errors.js
@@ -0,0 +1,5 @@
+let errores = {
+    "404": "Error al cargar la página, vuelva al inicio",
+    "403": "La página se ha mudado :(",
+    "503": "Hay Problemas con el servidor"
+};
--- pages/js/main.js
+++ pages/js/main.js
@@ -2,6 +2,10 @@ var font, fontSize = 400;
 var points;
 var automatas = [];
 var bg;
+
+var eContent = document.getElementById("content-code");
+eContent.innerHTML = errores[texto];
+
 $(function(){
     $("#drop-botton-menu").hover(function(){
         $('#drop-menu').addClass('dropdown');
@@ -11,19 +15,25 @@ $(function(){
         $('#drop-menu').addClass('dropdown-hidden');
         $('#drop-menu').removeClass('dropdown');
     });
+    $("#drop-botton-menu").click(function(){
+        $('#drop-menu').addClass('dropdown');
+        $('#drop-menu').removeClass('dropdown-hidden');
+    });
 });
 
 function preload() {
-  font = loadFont('../lib/AvenirNextLTPro-Demi.otf');
+  font = loadFont('lib/AvenirNextLTPro-Demi.otf');
   bg = loadImage('img/abstract.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(100);
-
-    x = (windowWidth / 2) - ((fontSize / 2) + 155);
-    y = (windowHeight / 2) + 100;
+    eContent.style.color =  color(random(200, 255), random(230, 255), random(60, 75));
+    fontSize = Math.abs(((windowWidth - fontSize) * 2) - 50);
+    fontSize = fontSize > 320 ? 320 : fontSize;
+    x = (windowWidth / 2) - ((fontSize / 2) + 125);
+    y = (windowHeight / 2) + 75;
 
     points = font.textToPoints(texto, x, y, fontSize, {
         sampleFactor: 0.12,
@@ -55,6 +65,15 @@ function draw() {
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+    background(100);
+    fontSize = Math.abs(((windowWidth - fontSize) * 2) - 50);
+    fontSize = fontSize > 320 ? 320 : fontSize;
+    x = (windowWidth / 2) - ((fontSize / 2) + 125);
+    y = (windowHeight / 2) + 75;
+
+    points = font.textToPoints(texto, x, y, fontSize, {
+        sampleFactor: 0.12,
+    });
     automatas = [];
     for (var i = 0; i < points.length; i++) {
         var pt = points[i];
